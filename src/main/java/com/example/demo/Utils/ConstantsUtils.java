3184fee824346cb592e565facff748a80b9341dd
AlejandroLozanoAparicio <alejandro.lozano.aparicio@estudiantat.upc.edu>
2023-04-07 11:45:24 +0200

    CONSTANT NAME FIXED

--- src/main/java/com/example/demo/Utils/ConstantsUtils.java
+++ src/main/java/com/example/demo/Utils/ConstantsUtils.java
@@ -1,7 +1,7 @@
 package com.example.demo.Utils;
 
 public class ConstantsUtils {
-    public static final String CHCECKBOX_TYPE = "checkbox";
+    public static final String CHECKBOX_TYPE = "checkbox";
     public static final String TEXT_TYPE = "text";
     public static final String RADIOBUTTON_TYPE = "radio";
 
