b97a20f019c80fc64300220b7d1fe76079f51d90
orio1pp <oriol.portell@estudiantat.upc.edu>
2023-06-11 19:16:53 +0200

    fix

--- src/main/java/com/example/demo/model/Options.java
+++ src/main/java/com/example/demo/model/Options.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Options {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long optionsId;z
+    private Long optionsId;
 
     @Column
     @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
